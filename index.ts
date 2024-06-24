d0f7156cd5642e73fc0bfcd19184da9c09136969
sidra batool <161121089+sidrabatool9@users.noreply.github.com>
2024-06-24 09:57:03 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "Enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operators to perform operation",
+    type: "list",
+    name: "operators",
+    choices: ["Addition", "Substraction", "Multiplication", "Division"],
+  },
+]);
+
+//conditional statement
+if(answer.operators === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operators === "Substraction") {
+    console.log (answer.firstNumber - answer.secondNumber);
+} else if (answer.operators === "Multiplication") {
+    console.log (answer.firstNumber * answer.secondNumber);
+} else if (answer.operators === "Division") {
+    console.log (answer.firstNumber / answer.secondNumber);
+} else {
+    console.log("please select valid operator");
+}
